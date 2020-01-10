4029ae0ca07aa39faa2b5c85daf434b099efc3b3
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-01-10 23:53:29 +0200

    add the user that create the tasks by observer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $tasks=[];
+        $tasks=Task::all();
         return view('home',['tasks'=>$tasks]);
     }
 }
