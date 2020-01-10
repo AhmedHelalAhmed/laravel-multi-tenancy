80b630f4db700001d0843530c4e40172bb8108e8
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-01-10 22:56:23 +0200

    Add custom template to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.dashboard.app')
 
 @section('content')
-hello
+    <div class="container">
+        <div class="row">
+
+
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">Welcome</h3>
+                    </div>
+
+                    <div class="box-body">
+                        here will be your tasks
+                    </div>
+                </div>
+
+
+        </div>
+    </div>
 @endsection
 
